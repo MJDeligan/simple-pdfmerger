3940afa16c53f339d85f9e7f024d1443f66e38a8
Marco <e11918513@student.tuwien.ac.at>
2021-02-02 00:53:27 +0100

    Corrected some formatting and added output on exceptions

--- pdfmerge.py
+++ pdfmerge.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
         "--nogui",
         action="store_true",
         dest="nogui",
-        help="Sets the program to open a file/directory dialog to select files/directories instead of passing them as command line args"
+        help="Sets the program to open a file/directory dialog to select\
+         files/directories instead of passing them as command line args"
     )
     parser.add_argument(
         "-out",
@@ -23,15 +24,27 @@ if __name__ == "__main__":
         default="merged.pdf",
         help='Name of the outputfile. Defaults to "merged.pdf"'
     )
-    parser.add_argument("-dir", nargs="?", metavar="directory", dest="dir", const=desktop_path, default=desktop_path, help="The directory to store the file in")
-    parser.add_argument("-pdfs", nargs="*", help="The pdfs you want to merge. Omitted when --gui is used.")
+    parser.add_argument(
+        "-dir",
+        nargs="?",
+        metavar="directory",
+        dest="dir",
+        const=desktop_path,
+        default=desktop_path,
+        help="The directory to store the file in"
+    )
+    parser.add_argument(
+        "-pdfs",
+        nargs="*",
+        help="The pdfs you want to merge. Omitted when --gui is used."
+    )
     args = parser.parse_args()
 
     # Merging section
     merger = PdfFileMerger()
     # use file dialog if option is set
     if args.nogui:
-        files = [f for f in args.pdfs if f.endswith('.pdf')] # filter non-pdfs
+        files = [f for f in args.pdfs if f.endswith('.pdf')]  # filter non-pdfs
         dir = args.dir
     else:
         # get file selection and filter non pdfs
@@ -45,17 +58,19 @@ if __name__ == "__main__":
     os.system("echo Merging:")
     for file in files:
         os.system(f"echo {file}")
-    out_name = args.out if args.out.endswith(".pdf") else f"{args.out}.pdf" # Append pdf extension to outputfile name if not there
+    # Append pdf extension to outputfile name if not there
+    out_name = args.out if args.out.endswith(".pdf") else f"{args.out}.pdf"
     try:
         for pdf in files:
             merger.append(pdf)
-    except:
+    except Exception as e:
+        os.system(f"echo {e}")
         os.system("echo Couldn't merge files")
-        system.exit(1)
+        sys.exit(1)
     try:
         merger.write(os.path.join(dir, out_name))
         os.system("echo Files merged!")
-    except:
+    except Exception as e:
+        os.system(f"echo {e}")
         os.system(f"echo Couldn't write to {os.path.join(dir, args.out)}")
     merger.close()
-
